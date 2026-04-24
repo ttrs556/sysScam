80a9bc6491655900a855bc43486df43e395f9d21
Sysachabut <marcosmelchior077@gmail.com>
2026-04-24 12:45:41 -0300

    Update pair.sh

--- pair.sh
+++ pair.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-read -p "Código conexão (5 dígitos): " conn
-read -p "Código pareamento: " pair
-read -p "Porta: " port
+read -p "Porta de pareamento: " pair_port
+read -p "Código de pareamento: " pair_code
+read -p "Porta de conexão: " connect_port
 
-adb pair localhost:$port
-adb connect localhost:$port
+adb pair localhost:$pair_port <<< "$pair_code"
+adb connect localhost:$connect_port
